0704f21eb8d15730cb837bc60cf6f5d46439051d
Owen Perez <owenps123@gmail.com>
2025-11-04 16:47:50 -0500

    refactor: Improve Prisma client instantiation and manage global instance

--- cineclub-next/prisma/lib.ts
+++ cineclub-next/prisma/lib.ts
@@ -1,5 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+type GlobalThis = typeof globalThis & {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined
+}
+
+const prisma = (globalThis as GlobalThis).prismaGlobal ?? prismaClientSingleton()
 
 export default prisma
+
+if (process.env.NODE_ENV !== 'production') {
+  (globalThis as GlobalThis).prismaGlobal = prisma
+}
